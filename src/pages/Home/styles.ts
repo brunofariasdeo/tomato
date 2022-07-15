5954ce3621ae5f3793a35118e45914144bf8d090
brunofariasdeo <jfarias@daitan.com>
2022-07-14 23:02:24 -0300

    Show dummy suggestions below TaskInput

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -85,6 +85,7 @@ const BaseInput = styled.input`
   height: 2.5rem;
   border: 0;
   border-bottom: 2px solid ${(props) => props.theme['gray-500']};
+  color: ${(props) => props.theme['gray-100']};
   font-weight: bold;
   font-size: 1.125rem;
   padding: 0 0.5rem;
@@ -101,6 +102,10 @@ const BaseInput = styled.input`
 
 export const TaskInput = styled(BaseInput)`
   flex: 1;
+
+  &::-webkit-calendar-picker-indicator {
+    display: none !important;
+  }
 `
 
 export const MinutesAmountInput = styled(BaseInput)`
